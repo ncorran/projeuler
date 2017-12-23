4d56c9c08986b4f47331e8c2cd891a44cdf21018
Nick C <nick_public@corran.org.uk>
2017-12-23 16:08:15 +0000

    Improve prime solving for q7, use for q10

--- q10.py
+++ q10.py
@@ -1,23 +1,30 @@
 primes = [2,3,5,7,11,13]
 n = 13
+maxprime = 2000000
 
-while len(primes) < 10001:
+while primes[-1] < maxprime:
     # Check only odd numbers
     n = n + 2
 
-    # Check for division by primes (other than 2)
     for i in range(1, len(primes)):
         p = primes[i]
         # no prime factor of n > sqrt(n) (other than n)
         if p*p > n:
             primes.append(n)
             break
-
+ 
         if n % p == 0:
             # found a divisor so not prime
             break
+         
     else:
         print("Gone wrong")
         exit()
 
-print("Prime[{}]: {}".format(len(primes), primes[10000]))
+print("Prime[{}]: {}".format(len(primes), primes[-1]))
+
+t = 0
+for i in range(len(primes) - 1):
+    t = t + primes[i]
+
+print("Total primes < {} = {}".format(maxprime, t))
